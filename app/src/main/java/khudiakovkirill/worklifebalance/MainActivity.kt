2a5cf34628b3c2eb5a1a2654d9ae3ac3a22ceb67
Kirill Khudiakov <khudiakovk@gmail.com>
2019-10-13 20:31:54 +0700

    implement LiveData for work-rest ratio

--- app/src/main/java/khudiakovkirill/worklifebalance/MainActivity.kt
+++ app/src/main/java/khudiakovkirill/worklifebalance/MainActivity.kt
@@ -2,17 +2,35 @@ package khudiakovkirill.worklifebalance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import khudiakovkirill.worklifebalance.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainActivityViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil
-            .setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        val seekBar = binding.seekBar
+//        seekBar.setOnSeekBarChangeListener(OnSeekBarChangeListener(viewModel))
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) =
+                viewModel.onSeekBarProgressChanged(seekBar, progress, fromUser)
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 }
+
--- app/src/main/java/khudiakovkirill/worklifebalance/MainActivityViewModel.kt
+++ app/src/main/java/khudiakovkirill/worklifebalance/MainActivityViewModel.kt
@@ -1,7 +1,28 @@
 package khudiakovkirill.worklifebalance
 
+import android.widget.SeekBar
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
 
+    private val _workRatio = MutableLiveData<Int>()
+    val workRatio: LiveData<Int>
+        get() = _workRatio
+
+    private val _restRatio = MutableLiveData<Int>()
+    val restRatio: LiveData<Int>
+        get() = _restRatio
+
+    init {
+        _workRatio.value = 5
+        _restRatio.value = 5
+    }
+
+    fun onSeekBarProgressChanged(seekBar: SeekBar?, progressValue: Int, fromUser: Boolean) {
+        _workRatio.value = progressValue
+        // We can confidently use !! operator because seekBar.max is defined in layout file.
+        _restRatio.value = seekBar!!.max - progressValue
+    }
 }
